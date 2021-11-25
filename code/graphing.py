8f7fe1e7a412f594245cbca7dd4bf700fea4e014
Suneha Bose <sbose2@ncsu.edu>
2021-11-25 17:15:06 -0500

    fixed error

--- code/graphing.py
+++ code/graphing.py
@@ -11,9 +11,10 @@ def addlabels(x, y):
 def visualize(total_text, monthly_budget):
 
     total_text_split = [line for line in total_text.split('\n') if line.strip() != '']
+    monthly_budget_str= ""
     for key, value in monthly_budget.items():
-        monthly_budget += str(key) + " $" + str(value) + "\n"
-    monthly_budget_split = [line for line in monthly_budget.split('\n') if line.strip() != '']
+        monthly_budget_str += str(key) + " $" + str(value) + "\n"
+    monthly_budget_split = [line for line in monthly_budget_str.split('\n') if line.strip() != '']
     categ_val = {}
     for i in total_text_split:
         a = i.split(' ')
@@ -30,7 +31,7 @@ def visualize(total_text, monthly_budget):
     y = list(categ_val.values())
 
     plt.bar(categ_val.keys(), categ_val.values(), color=[(1.00, 0, 0, 0.6), (0.2, 0.4, 0.6, 0.6), (0, 1.00, 0, 0.6), (1.00, 1.00, 0, 1.00)], edgecolor='blue')
-    plt.bar(monthly_budget_categ_val.keys(), monthly_budget_categ_val.values(), color=[(1.00, 0, 0, 0.6), (0.2, 0.4, 0.6, 0.6), (0, 1.00, 0, 0.6), (1.00, 1.00, 0, 1.00)], edgecolor='blue')
+    plt.bar(monthly_budget_categ_val.keys(), monthly_budget_categ_val.values())
     addlabels(x, y)
 
     plt.ylabel("Expenditure")
