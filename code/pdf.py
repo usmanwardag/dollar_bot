8875c01945e6e3f71a92edc9430f037754b5c6e9
Usman <usmanwardag@gmail.com>
2021-12-04 19:28:58 -0500

    PDF done, UI complete, fixed #38

--- code/pdf.py
+++ code/pdf.py
@@ -12,20 +12,26 @@ def run(message, bot):
         helper.read_json()
         chat_id = message.chat.id
         user_history = helper.getUserHistory(chat_id)
-        spend_total_str = ""
-        #if user_history is None:
-        #    raise Exception("Sorry! No spending records found!")
-        spend_total_str = "Here is your spending history : \nDATE, CATEGORY, AMOUNT\n----------------------\n"
-        if len(user_history) == 0:
-            spend_total_str = "Sorry! No spending records found!"
-        else:
-            for rec in user_history:
-                spend_total_str += str(rec) + "\n"
-        #bot.send_message(chat_id, spend_total_str)
         message = 'Alright. I just created a pdf of your expense history!'
         bot.send_message(chat_id, message)
-        plt.figure()
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        top = 0.8
+        if len(user_history) == 0:
+            plt.text(0.1, top, 'No record found!', horizontalalignment='left',
+                    verticalalignment='center', transform=ax.transAxes, fontsize=20)
+        for rec in user_history:
+            date, category, amount = rec.split(',')
+            date, time = date.split(' ')
+            print(date, category, amount)
+            rec_str = f'{amount}$ {category} expense on {date} at {time}'
+            plt.text(0, top, rec_str, horizontalalignment='left',
+                        verticalalignment='center', transform=ax.transAxes, fontsize=14,
+                        bbox=dict(facecolor='red', alpha=0.3))
+            top -= 0.15
+        plt.axis('off')
         plt.savefig('expense_history.pdf')
+        plt.close()
         bot.send_document(chat_id, open('expense_history.pdf', 'rb'))
     except Exception as e:
         logging.exception(str(e))
