fc546c57649420a8788e61fc81820dd63f2a4be7
Aditya Iyer <60314222+aiyer786@users.noreply.github.com>
2023-10-14 16:49:57 -0400

    Update test_budget.py

--- test/test_budget.py
+++ test/test_budget.py
@@ -1,95 +1,50 @@
-from mock import ANY
-import mock
-from mock.mock import patch
+import unittest
+import budget
+from unittest.mock import Mock, patch
 from telebot import types
-from code import budget
 
+class TestBudget(unittest.TestCase):
+    
+    def setUp(self):
+        self.bot = Mock()
+        self.message = Mock()
+        self.message.text = "Test text"
+        self.message.chat.id = 123456789
+    
+    @patch("budget.helper.getBudgetOptions")
+    @patch("budget.budget_update.run")
+    @patch("budget.budget_view.run")
+    @patch("budget.budget_delete.run")
+    def test_post_operation_selection(self, mock_delete_run, mock_view_run, mock_update_run, mock_get_options):
+        options = {"update": "Update Budget", "view": "View Budget", "delete": "Delete Budget"}
+        mock_get_options.return_value = options
+        
+        # Test budget update operation
+        self.message.text = options["update"]
+        budget.post_operation_selection(self.message, self.bot)
+        mock_update_run.assert_called_once_with(self.message, self.bot)
+        
+        # Test budget view operation
+        mock_update_run.reset_mock()
+        self.message.text = options["view"]
+        budget.post_operation_selection(self.message, self.bot)
+        mock_view_run.assert_called_once_with(self.message, self.bot)
+        
+        # Test budget delete operation
+        mock_view_run.reset_mock()
+        self.message.text = options["delete"]
+        budget.post_operation_selection(self.message, self.bot)
+        mock_delete_run.assert_called_once_with(self.message, self.bot)
+        
+        # Test invalid operation
+        mock_delete_run.reset_mock()
+        self.message.text = "Invalid Option"
+        with self.assertRaises(Exception) as context:
+            budget.post_operation_selection(self.message, self.bot)
+        self.assertEqual(
+            str(context.exception),
+            'Sorry I don\'t recognise this operation "Invalid Option"!',
+        )
 
-@patch("telebot.telebot")
-def test_run(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
-    message = create_message("hello from test run!")
-    budget.run(message, mc)
-    assert mc.reply_to.called_with(ANY, "Select Operation", ANY)
-
-
-@patch("telebot.telebot")
-def test_post_operation_selection_failing_case(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.send_message.return_value = True
-
-    mocker.patch.object(budget, "helper")
-    budget.helper.getBudgetOptions.return_value = {}
-
-    message = create_message("hello from budget test run!")
-    budget.post_operation_selection(message, mc)
-    mc.send_message.assert_called_with(11, "Invalid", reply_markup=mock.ANY)
-
-
-@patch("telebot.telebot")
-def test_post_operation_selection_update_case(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.send_message.return_value = True
-
-    mocker.patch.object(budget, "budget_update")
-    budget.budget_update.run.return_value = True
-
-    mocker.patch.object(budget, "helper")
-    budget.helper.getBudgetOptions.return_value = {
-        "update": "Add/Update",
-        "view": "View",
-        "delete": "Delete",
-    }
-
-    message = create_message("Add/Update")
-    budget.post_operation_selection(message, mc)
-    assert budget.budget_update.run.called
-
-
-@patch("telebot.telebot")
-def test_post_operation_selection_view_case(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.send_message.return_value = True
-
-    mocker.patch.object(budget, "budget_view")
-    budget.budget_view.run.return_value = True
-
-    mocker.patch.object(budget, "helper")
-    budget.helper.getBudgetOptions.return_value = {
-        "update": "Add/Update",
-        "view": "View",
-        "delete": "Delete",
-    }
-
-    message = create_message("View")
-    budget.post_operation_selection(message, mc)
-    assert budget.budget_view.run.called
-
-
-@patch("telebot.telebot")
-def test_post_operation_selection_delete_case(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.send_message.return_value = True
-
-    mocker.patch.object(budget, "budget_delete")
-    budget.budget_delete.run.return_value = True
-
-    mocker.patch.object(budget, "helper")
-    budget.helper.getBudgetOptions.return_value = {
-        "update": "Add/Update",
-        "view": "View",
-        "delete": "Delete",
-    }
-
-    message = create_message("Delete")
-    budget.post_operation_selection(message, mc)
-    assert budget.budget_delete.run.called
-
-
-def create_message(text):
-    params = {"messagebody": text}
-    chat = types.User(11, False, "test")
-    message = types.Message(1, None, None, chat, "text", params, "")
-    message.text = text
-    return message
+if __name__ == "__main__":
+    unittest.main()
