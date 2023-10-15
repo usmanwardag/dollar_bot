567d38cafe0af38aabf24ca5e6e02f49882548ff
Aditya Iyer <60314222+aiyer786@users.noreply.github.com>
2023-10-15 12:59:45 -0400

    Update test_budget_delete.py

--- test/test_budget_delete.py
+++ test/test_budget_delete.py
@@ -1,29 +1,43 @@
-from code import budget_delete
-from mock.mock import patch
-from telebot import types
+import unittest
+from unittest.mock import Mock, patch
+import budget_delete  
 
+class TestBudgetDelete(unittest.TestCase):
+    
+    @patch("budget_delete.helper")
+    def test_run(self, mock_helper):
+      
+        mock_message = Mock()
+        mock_message.chat.id = 123
+        
+  
+        mock_bot = Mock()
+        
+        # Mocking a user list that your function will interact with
+        user_list = {
+            "123": {
+                "budget": {
+                    "overall": 1000,
+                    "category": {"food": 500, "entertainment": 500}
+                }
+            }
+        }
+        
+  
+        mock_helper.read_json.return_value = user_list
 
-@patch("telebot.telebot")
-def test_run_normal_case(mock_telebot, mocker):
-    mc = mock_telebot.return_value
-    mc.send_message.return_value = True
+        budget_delete.run(mock_message, mock_bot)
+        
 
-    mocker.patch.object(budget_delete, "helper")
-    budget_delete.helper.read_json.return_value = {
-        "11": {"budget": {"budget": 10, "category": 100}}
-    }
-    budget_delete.helper.write_json.return_value = True
-
-    message = create_message("hello from testing")
-    budget_delete.run(message, mc)
-
-    # assert(mc.reply_to.called)
-    mc.send_message.assert_called_with(11, "Budget deleted!")
-
-
-def create_message(text):
-    params = {"messagebody": text}
-    chat = types.User(11, False, "test")
-    message = types.Message(1, None, None, chat, "text", params, "")
-    message.text = text
-    return message
+        mock_helper.read_json.assert_called_once()
+        
+ 
+        updated_user_list = {
+            "123": {"budget": {"overall": None, "category": None}}
+        }
+        mock_helper.write_json.assert_called_once_with(updated_user_list)
+        
+        mock_bot.send_message.assert_called_once_with(123, "Budget deleted!")
+    
+if __name__ == "__main__":
+    unittest.main()
